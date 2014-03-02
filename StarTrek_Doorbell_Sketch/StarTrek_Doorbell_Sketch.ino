00500af307ffbab0d670c7aa3b75cc11c7264be2
Tomdf <tomdflock@gmail.com>
2014-03-02 11:16:42 -0700

    Added comments to doorbell sketch

--- StarTrek_Doorbell_Sketch/StarTrek_Doorbell_Sketch.ino
+++ StarTrek_Doorbell_Sketch/StarTrek_Doorbell_Sketch.ino
@@ -1,11 +1,23 @@
+/* Written for ATtiny85 using Arduino IDE 1.0.5
+ This code monitors the voltage level of a doorbell's
+ speaker wire. If the voltage is higher than a set amount 
+ the ATtiny momentarily sets the pin connected to the chime's
+ PIR sensor's signal wire to high, as well as flashing a LED.
+ 3-1-14
+ Tom Flock
+ */
+
+//Define which wires are attached to what pin
 const int speakerPin = A1;
 const int ledPin = 0;
 const int pirPin = 1;
 
+//Create a variable to store the speaker's voltage value
 int speakerValue;
 
 void setup()
 {
+  //Set the pins as inputs or outputs
   pinMode(speakerPin, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(pirPin, OUTPUT);
@@ -13,16 +25,22 @@ void setup()
 
 void loop()
 {
+  //Read the voltage on the speaker wire pin as an analog
+  //value and store it in the variable speakerValue
   speakerValue = analogRead(speakerPin);
-    
+
+  //is the speaker voltage greater than zero?
   if (speakerValue >= 175){
-     digitalWrite(ledPin, HIGH);
-     digitalWrite(pirPin, HIGH);
-     delay(1000);
-     digitalWrite(ledPin, LOW);
-     digitalWrite(pirPin, LOW
-     
-     );
+    //yes it is!
+    digitalWrite(ledPin, HIGH); //turn the LED on
+    digitalWrite(pirPin, HIGH); //send 3.3V to the PIR sensor wire
+    delay(1000);                //pause for one second
+    digitalWrite(ledPin, LOW);  //turn the LED off
+    digitalWrite(pirPin, LOW);  //set the PIR sensor wire back to                        
+                                //zero volts
   }
-  delay(10);
+  
+  delay(10); //pause for 10 milliseconds before starting over
 }
+
+
